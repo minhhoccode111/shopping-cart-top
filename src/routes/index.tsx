a26e69413e1a3ff0037dacb241efa7f84fc16b99
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-12-15 21:43:13 +0700

    Fetch quote in home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Index: React.FC = () => {
+  const [flag, setFlag] = useState(0);
+  const [currentQuote, setCurrentQuote] = useState('');
+  const [currentAuthor, setCurrentAuthor] = useState('');
+  useEffect(() => {
+    const getQuote = async () => {
+      try {
+        const data = await fetch('https://api.quotable.io/random', { mode: 'cors' }).then((response) => response.json());
+        console.log(data.content);
+        setCurrentQuote(data.content);
+        setCurrentAuthor(data.author);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getQuote();
+  }, [flag]);
   return (
     <>
-      <section className="">
-        <h1>This is Index element or Default page</h1>
+      <section className="p-4">
+        <h1 className="">{currentQuote}</h1>
+        <h2 className="">{currentAuthor}</h2>
+        <button className="border rounded border-sky-500 text-sky-500 px-4 py-2" onClick={() => setFlag(Math.random())}>
+          New quote
+        </button>
         <Link className="underline" to={'shop'}>
           Shop now!
         </Link>
